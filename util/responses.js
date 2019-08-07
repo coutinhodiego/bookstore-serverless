9621c0972c034a6cb148784dad035003720aef6c
DIEGO BATISTA COUTINHO <diego.coutinho@MBMZN004198.mobly.local>
2019-08-07 18:51:54 -0300

    atualizando functions

--- util/responses.js
+++ util/responses.js
@@ -1,14 +1,14 @@
 
 module.exports.successResponse = (obj) => {
     return {
-        statusCode : 201,
-        body: JSON.stringify(obj)
+        statusCode: 201,
+        body: JSON.stringify(obj, null, 2)
     }
 }
 
 module.exports.errorResponse = (error) => {
     return {
         statusCode: 500,
-        body: JSON.stringify({ statusCode: 500, message: error.message })
+        body: JSON.stringify({ statusCode: 500, message: error.message }, null, 2)
     }
 }
